f3d2bab42e4edc1f0ba777fd6ccdfe0116d04d00
Fahz Kang Rinem <fahrifakhrullah@gmail.com>
2025-12-28 23:08:00 +0700

    Update scan.js

--- api/scan.js
+++ api/scan.js
@@ -1,1 +1,20 @@
-
+export default async function handler(req,res){
+try{
+const submit=await fetch("https://www.virustotal.com/api/v3/urls",{
+method:"POST",
+headers:{
+"x-apikey":process.env.VT_API_KEY,
+"Content-Type":"application/x-www-form-urlencoded"
+},
+body:new URLSearchParams({url:req.body.value})
+});
+const s=await submit.json();
+await new Promise(r=>setTimeout(r,4000));
+const report=await fetch(`https://www.virustotal.com/api/v3/analyses/${s.data.id}`,{
+headers:{"x-apikey":process.env.VT_API_KEY}
+});
+const d=await report.json();
+const st=d.data.attributes.stats;
+res.json(st);
+}catch{res.status(500).json({error:"scan error"})}
+}
